e41883392fb0cbe0cc7634d339ad8259ae6c0adf
Nailuj29Gaming <nailuj29gaming@gmail.com>
2020-05-26 19:32:27 -0400

    More semicolon fixes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -47,7 +47,7 @@ class _HomeState extends State<Home> {
               );
             }, 
             separatorBuilder: null, 
-            itemCount: countdownList.length;
+            itemCount: countdownList.length
           );
         },
       )
