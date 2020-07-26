734da235f672d17474913c437c0e7230c004f6e6
Nailuj29Gaming <nailuj29gaming@gmail.com>
2020-07-26 12:26:26 -0400

    open change event page on fab click

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:countdown/blocs/countdown_bloc.dart';
 import 'package:countdown/database/moor_db.dart';
+import 'package:countdown/pages/change_event.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -15,57 +16,71 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Countdown"),
-          actions: <Widget>[
-            /* IconButton(
+      appBar: AppBar(
+        title: const Text("Countdown"),
+        actions: <Widget>[
+          /* IconButton(
             icon: Icon(Icons.settings), 
             onPressed: () {},
           ), */
-            IconButton(
-              icon: Icon(Icons.calendar_today),
-              onPressed: () {},
-            )
-          ],
-        ),
-        body: BlocConsumer<CountdownBloc, List<Countdown>>(
-          builder: (context, countdownList) {
-            return ListView.separated(
-                itemBuilder: (context, index) {
-                  return Dismissible(
-                    key: Key(countdownList[index].id.toString()),
-                    child: ListTile(
-                      leading: Container(
-                        child: Column(
-                          children: <Widget>[
-                            Text(countdownList[index].date.month.toString(),
-                                style: TextStyle(
-                                  fontSize: 12,
-                                )),
-                            Text(countdownList[index].date.day.toString(),
-                                style: TextStyle(
-                                  fontSize: 24,
-                                ))
-                          ],
-                        ),
+          IconButton(
+            icon: Icon(Icons.calendar_today),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: BlocConsumer<CountdownBloc, List<Countdown>>(
+        builder: (context, countdownList) {
+          return ListView.separated(
+              itemBuilder: (context, index) {
+                return Dismissible(
+                  key: Key(countdownList[index].id.toString()),
+                  child: ListTile(
+                    leading: Container(
+                      child: Column(
+                        children: <Widget>[
+                          Text(countdownList[index].date.month.toString(),
+                              style: TextStyle(
+                                fontSize: 12,
+                              )),
+                          Text(countdownList[index].date.day.toString(),
+                              style: TextStyle(
+                                fontSize: 24,
+                              ))
+                        ],
                       ),
                     ),
-                    background: Container(
-                      color: Colors.red,
-                    ),
-                    secondaryBackground: Container(
-                      color: Colors.red,
-                    ),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return Divider(
-                    color: Colors.black,
-                  );
-                },
-                itemCount: countdownList == null ? 0 : countdownList.length);
-          },
-          listener: (BuildContext context, List<Countdown> countdowns) {},
-        ));
+                  ),
+                  background: Container(
+                    color: Colors.red,
+                  ),
+                  secondaryBackground: Container(
+                    color: Colors.red,
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return Divider(
+                  color: Colors.black,
+                );
+              },
+              itemCount: countdownList == null ? 0 : countdownList.length);
+        },
+        listener: (BuildContext context, List<Countdown> countdowns) {},
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          CountdownBloc bloc = BlocProvider.of<CountdownBloc>(context);
+          Countdown result = await Navigator.push<Countdown>(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ChangeEventScreen(
+                  initialDate: DateTime.now(),
+                ),
+              ));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
