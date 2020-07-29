974824be8e8b13d3f020fed879cd10e50b978bcc
Nailuj29Gaming <nailuj29gaming@gmail.com>
2020-07-29 19:14:04 -0400

    Added loading indicator

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:countdown/blocs/countdown_bloc.dart';
 import 'package:countdown/database/moor_db.dart';
 import 'package:countdown/pages/change_event.dart';
+import 'package:countdown/shared/date_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -24,40 +25,44 @@ class _HomeState extends State<Home> {
       ),
       body: BlocConsumer<CountdownBloc, List<Countdown>>(
         builder: (context, countdownList) {
-          return ListView.separated(
-              itemBuilder: (context, index) {
-                return Dismissible(
-                  key: Key(countdownList[index].id.toString()),
-                  child: ListTile(
-                    leading: Container(
-                      child: Column(
-                        children: <Widget>[
-                          Text(countdownList[index].date.month.toString(),
-                              style: TextStyle(
-                                fontSize: 12,
-                              )),
-                          Text(countdownList[index].date.day.toString(),
-                              style: TextStyle(
-                                fontSize: 24,
-                              ))
-                        ],
+          return countdownList == null
+              ? CircularProgressIndicator(
+                  value: null,
+                )
+              : ListView.separated(
+                  itemBuilder: (context, index) {
+                    return Dismissible(
+                      key: Key(countdownList[index].id.toString()),
+                      child: ListTile(
+                        leading: Container(
+                          child: Column(
+                            children: <Widget>[
+                              Text(DateUtils.month(countdownList[index].date),
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                  )),
+                              Text(DateUtils.day(countdownList[index].date),
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                  ))
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  background: Container(
-                    color: Colors.red,
-                  ),
-                  secondaryBackground: Container(
-                    color: Colors.red,
-                  ),
-                );
-              },
-              separatorBuilder: (context, index) {
-                return Divider(
-                  color: Colors.black,
-                );
-              },
-              itemCount: countdownList == null ? 0 : countdownList.length);
+                      background: Container(
+                        color: Colors.red,
+                      ),
+                      secondaryBackground: Container(
+                        color: Colors.red,
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return Divider(
+                      color: Colors.black,
+                    );
+                  },
+                  itemCount: countdownList == null ? 0 : countdownList.length);
         },
         listener: (BuildContext context, List<Countdown> countdowns) {},
       ),
