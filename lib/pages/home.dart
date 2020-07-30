7414a6a92a8613bcc11701c39ca812528635aa07
Nailuj29Gaming <nailuj29gaming@gmail.com>
2020-07-30 18:29:25 -0400

    added loading indicator

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -30,69 +30,75 @@ class _HomeState extends State<Home> {
       body: StreamBuilder<List<Countdown>>(
           stream: database.watchCountdowns(),
           builder: (context, snapshot) {
-            final countdownList = snapshot.data ?? List();
-            return ListView.separated(
-                padding: EdgeInsets.all(0.0),
-                itemBuilder: (context, index) {
-                  int _daysRemaining = countdownList[index]
-                      .date
-                      .difference(DateTime.now())
-                      .inDays;
-                  return Dismissible(
-                    key: Key(countdownList[index].id.toString()),
-                    onDismissed: (direction) {
-                      CountdownBloc bloc = BlocProvider.of(context);
-                      bloc.add(CountdownEvent.delete(countdownList[index]));
-                    },
-                    child: ListTile(
-                      leading: Column(
-                        children: <Widget>[
-                          SizedBox(height: 10.0),
-                          Text(DateUtils.month(countdownList[index].date),
-                              style: TextStyle(
-                                fontSize: 12,
-                              )),
-                          Text(DateUtils.day(countdownList[index].date),
-                              style: TextStyle(
-                                fontSize: 24,
-                              ))
-                        ],
-                      ),
-                      title: Container(
-                        child: Text(_daysRemaining > 0
-                            ? "${_daysRemaining + 1} day${_daysRemaining == 1 ? "" : "s"} until ${countdownList[index].name}"
-                            : _daysRemaining == 0
-                                ? "${countdownList[index].name} is today! 🎉"
-                                : "${countdownList[index].name} was ${_daysRemaining.abs()} day${_daysRemaining == -1 ? "" : "s"} ago"),
-                      ),
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) {
-                            return ChangeEventScreen(
-                              countdown: countdownList[index],
-                            );
+            final countdownList = snapshot.data;
+            return countdownList == null
+                ? Center(
+                    child: CircularProgressIndicator(
+                      value: null,
+                    ),
+                  )
+                : ListView.separated(
+                    padding: EdgeInsets.all(0.0),
+                    itemBuilder: (context, index) {
+                      int _daysRemaining = countdownList[index]
+                          .date
+                          .difference(DateTime.now())
+                          .inDays;
+                      return Dismissible(
+                        key: Key(countdownList[index].id.toString()),
+                        onDismissed: (direction) {
+                          database.deleteCountdown(countdownList[index]);
+                        },
+                        child: ListTile(
+                          leading: Column(
+                            children: <Widget>[
+                              SizedBox(height: 10.0),
+                              Text(DateUtils.month(countdownList[index].date),
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                  )),
+                              Text(DateUtils.day(countdownList[index].date),
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                  ))
+                            ],
+                          ),
+                          title: Container(
+                            child: Text(_daysRemaining > 0
+                                ? "${_daysRemaining + 1} day${_daysRemaining == 1 ? "" : "s"} until ${countdownList[index].name}"
+                                : _daysRemaining == 0
+                                    ? "${countdownList[index].name} is today! 🎉"
+                                    : "${countdownList[index].name} was ${_daysRemaining.abs()} day${_daysRemaining == -1 ? "" : "s"} ago"),
+                          ),
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) {
+                                return ChangeEventScreen(
+                                  countdown: countdownList[index],
+                                );
+                              },
+                            ));
                           },
-                        ));
-                      },
-                    ),
-                    background: Container(
-                      alignment: AlignmentDirectional.centerStart,
-                      color: Colors.red,
-                      child: Icon(Icons.delete_outline),
-                      padding: EdgeInsets.all(16.0),
-                    ),
-                    secondaryBackground: Container(
-                      alignment: AlignmentDirectional.centerEnd,
-                      color: Colors.red,
-                      child: Icon(Icons.delete_outline),
-                      padding: EdgeInsets.all(16.0),
-                    ),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return Divider(color: Colors.grey[800]);
-                },
-                itemCount: countdownList == null ? 0 : countdownList.length);
+                        ),
+                        background: Container(
+                          alignment: AlignmentDirectional.centerStart,
+                          color: Colors.red,
+                          child: Icon(Icons.delete_outline),
+                          padding: EdgeInsets.all(16.0),
+                        ),
+                        secondaryBackground: Container(
+                          alignment: AlignmentDirectional.centerEnd,
+                          color: Colors.red,
+                          child: Icon(Icons.delete_outline),
+                          padding: EdgeInsets.all(16.0),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return Divider(color: Colors.grey[800]);
+                    },
+                    itemCount:
+                        countdownList == null ? 0 : countdownList.length);
           }),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
