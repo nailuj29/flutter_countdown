da0867f9d482881e64fb890d8e9f44210dd85c55
Nailuj29Gaming <nailuj29gaming@gmail.com>
2020-07-30 18:34:52 -0400

    more cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:countdown/database/moor_db.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:countdown/pages/home.dart';
 import 'package:provider/provider.dart';
 
-import 'blocs/countdown_bloc_delegate.dart';
-
 void main() async {
-  BlocSupervisor.delegate = CountdownBlocDelegate();
   runApp(MyApp());
 }
 
