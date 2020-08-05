a2f5be223508fc660c24702a7df30c5603f7ce23
Nailuj29Gaming <nailuj29gaming@gmail.com>
2020-08-05 15:38:15 -0400

    remove non-existent import

--- lib/pages/change_event.dart
+++ lib/pages/change_event.dart
@@ -1,7 +1,6 @@
 import 'package:countdown/database/moor_db.dart';
 import 'package:eyro_toast/eyro_toast.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
